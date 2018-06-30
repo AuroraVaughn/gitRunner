3bcb2862fedfadafbc3f8f296d589bc85a44f67e
Aurora Vaughn <avaughn.git@gmail.com>
2018-06-29 21:20:10 -0500

    deploy ready, reorganized and express added

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
-var http = require('http');
+const express = require('express');
+const app = express();
+const path = require('path');
+// set the port of our application
+// process.env.PORT lets the port be set by Heroku
+const port = process.env.PORT || 8080;
 
-var nStatic = require('node-static');
+// make express look in the public directory for assets (css/js/img)
+app.use(express.static(__dirname + '/public'));
 
-var fileServer = new nStatic.Server('./public');
+// set the home page route
+app.get('/', function (req, res) {
 
-http.createServer(function (req, res) {
+  // ejs render automatically looks in the views folder
+  res.sendFile(path.join(__dirname + '/index.html'));
+});
 
-  fileServer.serve(req, res);
-
-}).listen(5000);
+app.listen(port, function () {
+  console.log('Our app is running on http://localhost:' + port);
+});
